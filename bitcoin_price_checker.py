58f78e3217c553645f400109bca1a862e83027d3
Oliver Grimm <olivergrimm@fastmail.fm>
2018-12-16 14:13:00 +0900

    added time example and modified bitcoin example to print current time

--- bitcoin_price_checker.py
+++ bitcoin_price_checker.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import requests
+import datetime
 
 price_url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
 data = requests.get(price_url).json()
 price_in_usd = data['bpi']['USD']['rate']
 
-print("Bitcoin price is currently at $" + price_in_usd + " USD")
+time = datetime.datetime.now()
+print("It is " + time.strftime("%X") + " and Bitcoin price is currently at $" + price_in_usd + " USD")
---
+++ time.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import datetime
+
+time = datetime.datetime.now()
+print("The current time is " + time.strftime("%X"))
